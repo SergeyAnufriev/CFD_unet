711a5b9d030459f7f87adee10e20e5b1fb3a90ea
Sergey Anufriev <anufriiev.sergii@gmail.com>
2022-07-25 14:59:24 +0100

    bug fix

--- data_.py
+++ data_.py
@@ -114,7 +114,7 @@ def node_data(df):
 
     node_dict_ = {x:y for x,y in zip(df['node_num'],list(range(len(df))))}
     coord_velocities = torch.tensor(df[['x','y','u_x','u_y']].values,dtype=torch.double)
-    nodes_types = one_hot(torch.tensor(df['node_type'],dtype=torch.double))
+    nodes_types = one_hot(torch.tensor(df['node_type']))
     X_input = torch.cat([coord_velocities,nodes_types],dim=1)
     X_output = torch.tensor(df[['P','v_x','v_y']].values,dtype=torch.double)
 
